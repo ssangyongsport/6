67b5648f0842cdcc438b37c0fce7b272625002b3
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-12-26 21:33:09 +0800

    Update [slug].tsx

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -16,139 +16,48 @@ import ShareWidget from 'views/SingleArticlePage/ShareWidget';
 import StructuredDataHead from 'views/SingleArticlePage/StructuredDataHead';
 import { Posts, PostsDocument, Query } from '.tina/__generated__/types';
 
-export default function SingleArticlePage(props: InferGetStaticPropsType<typeof getStaticProps>) {
-  const contentRef = useRef<HTMLDivElement | null>(null);
-  const [readTime, setReadTime] = useState('');
+export default function Blog({posts}){
+    return <main>
+        {posts.map(post => {
+            //extract slug and frontmatter
+            const {slug, frontmatter} = post
+            //extract frontmatter properties
+            const {title, author, category, date, bannerImage, tags} = frontmatter
 
-  useEffect(() => {
-    calculateReadTime();
-    lazyLoadPrismTheme();
-
-    function calculateReadTime() {
-      const currentContent = contentRef.current;
-      if (currentContent) {
-        setReadTime(getReadTime(currentContent.textContent || ''));
-      }
-    }
-
-    function lazyLoadPrismTheme() {
-      const prismThemeLinkEl = document.querySelector('link[data-id="prism-theme"]');
-
-      if (!prismThemeLinkEl) {
-        const headEl = document.querySelector('head');
-        if (headEl) {
-          const newEl = document.createElement('link');
-          newEl.setAttribute('data-id', 'prism-theme');
-          newEl.setAttribute('rel', 'stylesheet');
-          newEl.setAttribute('href', '/prism-theme.css');
-          newEl.setAttribute('media', 'print');
-          newEl.setAttribute('onload', "this.media='all'; this.onload=null;");
-          headEl.appendChild(newEl);
-        }
-      }
-    }
-  }, []);
-
-  const { slug, data } = props;
-  const content = data.getPostsDocument.data.body;
-
-  if (!data) {
-    return null;
-  }
-  const { title, description, date, tags, imageUrl } = data.getPostsDocument.data as NonNullableChildrenDeep<Posts>;
-  const meta = { title, description, date: date, tags, imageUrl, author: '' };
-  const formattedDate = formatDate(new Date(date));
-  const absoluteImageUrl = imageUrl.replace(/\/+/, '/');
-  return (
-    <>
-      <Head>
-        <noscript>
-          <link rel="stylesheet" href="/prism-theme.css" />
-        </noscript>
-      </Head>
-      <OpenGraphHead slug={slug} {...meta} />
-      <StructuredDataHead slug={slug} {...meta} />
-      <MetadataHead {...meta} />
-      <CustomContainer id="content" ref={contentRef}>
-        <ShareWidget title={title} slug={slug} />
-        <Header title={title} formattedDate={formattedDate} imageUrl={absoluteImageUrl} readTime={readTime} />
-        <MDXRichText content={content} />
-      </CustomContainer>
-    </>
-  );
+            //JSX for individual blog listing
+            return <article key={title}>
+                <Link href={`/posts/${slug}`}>
+                    <h1>{title}</h1>
+                </Link>
+                <h3>{author}</h3>
+                <h3>{date}</h3>
+            </article>
+        })}
+    </main>
 }
 
-export async function getStaticPaths() {
-  const postsListData = await staticRequest({
-    query: `
-      query PostsSlugs{
-        getPostsList{
-          edges{
-            node{
-              sys{
-                basename
-              }
-            }
-          }
-        }
-      }
-    `,
-    variables: {},
-  });
 
-  if (!postsListData) {
+//Generating the Static Props for the Blog Page
+export async function getStaticProps(){
+    // get list of files from the posts folder
+    const files = fs.readdirSync('posts');
+
+    // get frontmatter & slug from each post
+    const posts = files.map((fileName) => {
+        const slug = fileName.replace('.md', '');
+        const readFile = fs.readFileSync(`posts/${fileName}`, 'utf-8');
+        const { data: frontmatter } = matter(readFile);
+
+        return {
+          slug,
+          frontmatter,
+        };
+    });
+
+    // Return the pages static props
     return {
-      paths: [],
-      fallback: false,
+        props: {
+          posts,
+        },
     };
-  }
-
-  type NullAwarePostsList = { getPostsList: NonNullableChildrenDeep<Query['getPostsList']> };
-  return {
-    paths: (postsListData as NullAwarePostsList).getPostsList.edges.map((edge) => ({
-      params: { slug: normalizePostName(edge.node.sys.basename) },
-    })),
-    fallback: false,
-  };
 }
-
-function normalizePostName(postName: string) {
-  return postName.replace('.mdx', '');
-}
-
-export async function getStaticProps({ params }: GetStaticPropsContext<{ slug: string }>) {
-  const { slug } = params as { slug: string };
-  const variables = { relativePath: `${slug}.mdx` };
-  const query = `
-    query BlogPostQuery($relativePath: String!) {
-      getPostsDocument(relativePath: $relativePath) {
-        data {
-          title
-          description
-          date
-          tags
-          imageUrl
-          body
-        }
-      }
-    }
-  `;
-
-  const data = (await staticRequest({
-    query: query,
-    variables: variables,
-  })) as { getPostsDocument: PostsDocument };
-
-  return {
-    props: { slug, variables, query, data },
-  };
-}
-
-const CustomContainer = styled(Container)`
-  position: relative;
-  max-width: 90rem;
-  margin: 10rem auto;
-  ${media('<=tablet')} {
-    margin: 5rem auto;
-  }
-`;
